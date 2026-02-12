d7d42f3beb2340a046f646f6f6da5151ce558753
Niran Chaudahry <Niranchaudhary314@gmail.com>
2026-02-12 16:48:35 +0545

    new program added

---
+++ 1hello.py
@@ -0,0 +1,12 @@
+# print hello 5 times
+i=1
+while i<=5:
+    print("Hello")
+    i+=1
+    
+print(i)  #====>>>returns 6
+
+# another way
+for i in range(1,6):
+    print("hello")
+print(i)  #===>>>returns 5
