f1680d82d60c0da3ef7748dd9de884bc913f4ae8
Niran Chaudahry <Niranchaudhary314@gmail.com>
2026-02-16 17:00:40 +0545

    1 to 5 and 5 to 1

--- 2loop.py
+++ 2loop.py
@@ -1,4 +1,20 @@
 i=1
 while i<=10:
     print ("Hello Nepal",i)
-    i+=1
+    i+=1 
+
+
+
+#print numbers from 1 to 5
+j=1
+while j<=5:
+    print(j)
+    j+=1
+
+#print numbers from 5 to 1
+k= 5
+while k>=1:
+    print(k)
+    k-=1
+
+print("loop ended")
